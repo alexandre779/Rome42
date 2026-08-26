6ee789d23152f378d06e9af5fd6e26f12edc55b3
alexandre779 <alexandre@fiveaday.fr>
2026-08-26 22:22:39 +0200

    chore: refresh v51 app cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='rome42-v50';
+const C='rome42-v51';
 const CORE=['./','index.html','manifest.webmanifest','icons/r42-192.png','icons/r42-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('rome42-')&&k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
